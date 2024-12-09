4a79c738862381174e8b04f2d583ea7653f5b707
wendelfrota <wendelalves898@gmail.com>
2024-12-09 15:11:34 -0300

    fix: rename user routes file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userRoutes = require('./routes/userRouter')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 const port = 8080;
