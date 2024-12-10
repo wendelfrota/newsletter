6c129dae962a151cb0ddd8c24bad699ca88aa1b5
wendelfrota <wendelalves898@gmail.com>
2024-12-10 09:54:57 -0300

    fix: resolve broken EJS layout and add middleware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userRoutes = require('./src/routes/userRoutes')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(userRoutes);
 
