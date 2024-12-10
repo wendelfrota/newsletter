1fca0ba74e61ab1bc2823bb9f5fe0dc180b42863
wendelfrota <wendelalves898@gmail.com>
2024-12-09 23:34:12 -0300

    refactor: add node_env param into configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then
-    echo "$0 database: <name> <user> <password> <host> <dialect>"
+if [ $# -ne 6 ]; then
+    echo "$0 database: <name> <user> <password> <host> <dialect> <node_env>"
     exit 1
 fi
 
@@ -12,5 +12,6 @@ echo "DB_USER=$2" >> .env
 echo "DB_PASS=$3" >> .env
 echo "DB_HOST=$4" >> .env
 echo "DB_DIALECT=$5" >> .env
+echo "NODE_ENV=$6" >> .env
 
 echo ".env file created"
