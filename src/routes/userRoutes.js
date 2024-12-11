918ca18229a96620979eb08c863c85f72a00d833
wendelfrota <wendelalves898@gmail.com>
2024-12-11 17:48:58 -0300

    rename: homeController -> routerController

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-const homeController = require('../controllers/homeController');
+const homeController = require('../controllers/routerController');
 const router = express.Router();
 
 router.get('/', homeController.getHome);
