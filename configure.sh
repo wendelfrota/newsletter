e7c87217e0785e5beeb6b438dfb49d9ac6d74d48
wendelfrota <wendelalves898@gmail.com>
2024-12-15 21:18:07 -0300

    feat: enhance .env generation script

--- configure.sh
+++ configure.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]; then
-    echo "$0 database: <name> <user> <password> <host> <dialect> <node_env>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <database_name> <user> <password> <host> <dialect> <node_env>"
+    echo "Example: $0 my_database my_user my_password localhost mysql development"
     exit 1
 fi
 
-touch .env
+DB_NAME=$1
+DB_USER=$2
+DB_PASS=$3
+DB_HOST=$4
+DB_DIALECT=$5
+NODE_ENV=$6
 
-echo "DB_NAME=$1" >> .env
-echo "DB_USER=$2" >> .env
-echo "DB_PASS=$3" >> .env
-echo "DB_HOST=$4" >> .env
-echo "DB_DIALECT=$5" >> .env
-echo "NODE_ENV=$6" >> .env
+{
+    echo "DB_NAME=$DB_NAME"
+    echo "DB_USER=$DB_USER"
+    echo "DB_PASS=$DB_PASS"
+    echo "DB_HOST=$DB_HOST"
+    echo "DB_DIALECT=$DB_DIALECT"
+    echo "NODE_ENV=$NODE_ENV"
+} > .env
 
-echo ".env file created"
+echo ".env file successfully created"
+echo "---------------------------------"
+cat .env
+echo "---------------------------------"
