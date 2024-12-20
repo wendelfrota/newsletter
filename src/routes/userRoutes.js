e93eb91e9fa62c87eb7af740ea092e0eeed8cb45
wendelfrota <wendelalves898@gmail.com>
2024-12-20 12:03:08 -0300

    feat: add authentication checks for routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,16 +3,33 @@ const userController = require('../controllers/userController');
 const routerController = require('../controllers/routerController');
 const router = express.Router();
 
-router.get('/', routerController.getHome);
-router.get('/login', routerController.getLogin);
-router.get('/signup', routerController.getSignUp);
-router.get('/cookies', (req, res) => res.json(req.session));
+function isAuthenticated(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
 
-router.post('/signup', userController.createUser);
-router.post('/login', userController.loginUser);
+function isAlreadyAuthenticated(req, res, next) {
+    if (req.session && req.session.user) {
+        return res.redirect('/');
+    } else {
+        return next();
+    }
+}
+
+
+router.get('/', routerController.getHome);
+router.get('/login', isAlreadyAuthenticated, routerController.getLogin);
+router.get('/signup', isAlreadyAuthenticated, routerController.getSignUp);
+router.get('/cookies', isAuthenticated, (req, res) => res.json(req.session));
+
+router.post('/signup', isAlreadyAuthenticated, userController.createUser);
+router.post('/login', isAlreadyAuthenticated, userController.loginUser);
 
 // Using GET temporarily for debugging (will replace with proper method)
-router.get('/users', userController.getUsers);
+router.get('/users', isAuthenticated, userController.getUsers);
 
 router.use(routerController.get404)
 
